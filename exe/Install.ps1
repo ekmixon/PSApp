e11eca0f3c3a17af09f06f9d927dd6ca624a53d5
Chris Gerke <chris.gerke@water.wa.gov.au>
2020-10-23 09:55:02 +0800

    Nicer

--- exe/Install.ps1
+++ exe/Install.ps1
@@ -1,12 +1,20 @@
-##*===============================================
-##* VARIABLE DECLARATION
-##*===============================================
-[string]$InstallerFile = "filename.exe"
-[array]$InstallerArgs = @(
+## Variables: Environment
+[string]$DateString = (Get-Date).ToString("yyyy-MM-dd-hhmmss")
+[string]$SrcPath = $(Split-Path -Parent $PSCommandPath)
+[string]$TmpPath = "$env:TEMP"
+
+## Variables: Installer specific
+[string]$SrcFile = "filename.exe"
+[array]$InstallOptions = @(
     '/S',
     "/f1'$ScriptPath\answer.iss'"
 )
 
-## <Perform Installation tasks here>
-[string]$ScriptPath = $(Split-Path -Parent $PSCommandPath)
-Start-Process "$InstallerFile" -ArgumentList $InstallerArgs -WorkingDirectory "$ScriptPath" -NoNewWindow -Wait
+## Perform installation tasks here
+Start-Transcript -Path "$TmpPath\$SrcFile.$DateString.log"
+Try {
+    Start-Process "$InstallerFile" -ArgumentList $InstallOptions -WorkingDirectory "$SrcPath" -NoNewWindow -Wait
+} Catch {
+    Write-Error -Message "Error"
+}
+Stop-Transcript
